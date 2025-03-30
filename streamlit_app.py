f2582d2d0aae4b7ca855c3367ec5f841f26cccd0
João José Bentivi <jjbentivi@hotmail.com>
2025-03-30 04:12:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 #from openai import OpenAI
 import requests
+import warnings
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+
+# Suppress Streamlit's ScriptRunContext warning
+warnings.filterwarnings("ignore", message="missing ScriptRunContext")
+
+# Configuração inicial da página
+st.set_page_config(page_title="O Pensador Desktop", layout="wide")
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
