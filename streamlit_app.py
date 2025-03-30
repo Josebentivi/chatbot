988ec818302e3dda686b411d5c1b3e6566c28d97
João José Bentivi <jjbentivi@hotmail.com>
2025-03-30 04:07:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,111 +20,115 @@ else:
     coluna1, coluna2, coluna3  = st.columns([1, 5, 1])
     # Create an OpenAI client.
     #client = OpenAI(api_key=openai_api_key)
+    with coluna1:
+        st.image("https://cdn.pixabay.com/photo/2017/08/30/02/53/socrates-2696229_1280.jpg", caption="Sócrates", width=200)
+    with coluna2:
+        # Create a session state variable to store the chat messages. This ensures that the
+        # messages persist across reruns.
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        if "active_mode" not in st.session_state:
+            st.session_state.active_mode = "none"  # Valores possíveis: "none", "artigos", "pensadores"
+        if "selected_thinker" not in st.session_state:
+            st.session_state.selected_thinker = None
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    if "active_mode" not in st.session_state:
-        st.session_state.active_mode = "none"  # Valores possíveis: "none", "artigos", "pensadores"
-    if "selected_thinker" not in st.session_state:
-        st.session_state.selected_thinker = None
+        # Display the existing chat messages via `st.chat_message`.
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+                if message["referencia"]:
+                    if st.button("Referência"):
+                        st.markdown("Why hello there")
+                    else:
+                        st.markdown("Goodbye")
+                if message["argumentacao"]:
+                    if st.button("Argumentação"):
+                        st.markdown("Why hello there")
+                    else:
+                        st.markdown("Goodbye")
+        if st.session_state.active_mode != "artigos":
+            st.info("Artigos desativados", icon="⚠️")
+        if st.session_state.active_mode != "pensadores":
+            st.info("#####Pensadores desativados", icon="⚠️")
+        # Create a chat input field to allow the user to enter a message. This will display
+        # automatically at the bottom of the page.
+        if prompt := st.chat_input("Em que eu posso te ajudar?"):
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-            if message["referencia"]:
-                if st.button("Referência"):
-                    st.markdown("Why hello there")
-                else:
-                    st.markdown("Goodbye")
-            if message["argumentacao"]:
-                if st.button("Argumentação"):
-                    st.markdown("Why hello there")
-                else:
-                    st.markdown("Goodbye")
-    if st.session_state.active_mode != "artigos":
-        st.info("Artigos desativados", icon="⚠️")
-    if st.session_state.active_mode != "pensadores":
-        st.info("#####Pensadores desativados", icon="⚠️")
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("Em que eu posso te ajudar?"):
+            # Store and display the current prompt.
+            st.session_state.messages.append({"role": "user", "content": prompt,"referencia": False,"argumentacao": False})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt,"referencia": False,"argumentacao": False})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+            
+            # Generate a response using the OpenAI API.
+            #stream = client.chat.completions.create(
+            #    model="gpt-3.5-turbo",
+            #    messages=[
+            #        {"role": m["role"], "content": m["content"]}
+            #        for m in st.session_state.messages
+            #    ],
+            #    stream=True,
+            #)
 
-        
-        # Generate a response using the OpenAI API.
-        #stream = client.chat.completions.create(
-        #    model="gpt-3.5-turbo",
-        #    messages=[
-        #        {"role": m["role"], "content": m["content"]}
-        #        for m in st.session_state.messages
-        #    ],
-        #    stream=True,
-        #)
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        #with st.chat_message("assistant"):
-        #    response = st.write_stream(stream)
-        
-        try:
-            # Chamada à API (substitua a URL pelo endpoint real)
-            url = "http://52.2.202.37/mapamental/"
-            data = {"cliente": "string",
-                    "produto": "string",
-                    }
-            response = requests.post(url, json=data, timeout=5*60)
-            if response.status_code == 200:  
-                saida = response.json()["saida"]
-                print(saida)
-                erro = response.json()["erro"]
-                print(erro)
-            else:  
-                print("Erro na requisição")
-                print(response.status_code)
-                print(response.text) 
-                st.stop()   
-        except Exception as e:
-            dados = {"mensagem": "Erro ao processar a mensagem."}
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = "Certo"
-            st.markdown("Certo")
-        if st.session_state.active_mode == "artigos":
-            st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": False})
-        if st.session_state.active_mode == "pensadores":
-            st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": True})
-        st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": False})
-
-    col1, col2, col3, col4  = st.columns([1, 1, 1, 1])
-    with col1:
-        if st.button("Artigos Científicos"):
-            # Ativa o modo "artigos" ou desativa se já estiver ativo
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            #with st.chat_message("assistant"):
+            #    response = st.write_stream(stream)
+            
+            try:
+                # Chamada à API (substitua a URL pelo endpoint real)
+                url = "http://52.2.202.37/mapamental/"
+                data = {"cliente": "string",
+                        "produto": "string",
+                        }
+                response = requests.post(url, json=data, timeout=5*60)
+                if response.status_code == 200:  
+                    saida = response.json()["saida"]
+                    print(saida)
+                    erro = response.json()["erro"]
+                    print(erro)
+                else:  
+                    print("Erro na requisição")
+                    print(response.status_code)
+                    print(response.text) 
+                    st.stop()   
+            except Exception as e:
+                dados = {"mensagem": "Erro ao processar a mensagem."}
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            with st.chat_message("assistant"):
+                response = "Certo"
+                st.markdown("Certo")
             if st.session_state.active_mode == "artigos":
-                st.session_state.active_mode = "none"
-            else:
-                st.session_state.active_mode = "artigos"
-    with col2:
-        if st.button("Pensadores"):
-            # Ativa o modo "pensadores" ou desativa se já estiver ativo
+                st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": False})
             if st.session_state.active_mode == "pensadores":
-                st.session_state.active_mode = "none"
-            else:
-                st.session_state.active_mode = "pensadores"
-                if st.session_state.selected_thinker is None:
-                    st.session_state.selected_thinker = "Sócrates"
-                    
-    if st.session_state.active_mode == "pensadores":
-        st.session_state.selected_thinker = st.selectbox(
-            "Selecione o pensador:",
-            options=["Sócrates", "Platão", "Aristóteles", "Descartes"],
-            index=["Sócrates", "Platão", "Aristóteles", "Descartes"].index(st.session_state.selected_thinker)
-        )
-    
+                st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": True})
+            st.session_state.messages.append({"role": "assistant", "content": response,"referencia": True,"argumentacao": False})
+
+        col1, col2, col3, col4  = st.columns([1, 1, 1, 1])
+        with col1:
+            if st.button("Artigos Científicos"):
+                # Ativa o modo "artigos" ou desativa se já estiver ativo
+                if st.session_state.active_mode == "artigos":
+                    st.session_state.active_mode = "none"
+                else:
+                    st.session_state.active_mode = "artigos"
+        with col2:
+            if st.button("Pensadores"):
+                # Ativa o modo "pensadores" ou desativa se já estiver ativo
+                if st.session_state.active_mode == "pensadores":
+                    st.session_state.active_mode = "none"
+                else:
+                    st.session_state.active_mode = "pensadores"
+                    if st.session_state.selected_thinker is None:
+                        st.session_state.selected_thinker = "Sócrates"
+                        
+        if st.session_state.active_mode == "pensadores":
+            st.session_state.selected_thinker = st.selectbox(
+                "Selecione o pensador:",
+                options=["Sócrates", "Platão", "Aristóteles", "Descartes"],
+                index=["Sócrates", "Platão", "Aristóteles", "Descartes"].index(st.session_state.selected_thinker)
+            )
+        
+    with coluna3:
+        st.image("https://cdn.pixabay.com/photo/2017/08/30/02/53/socrates-2696229_1280.jpg", caption="Sócrates", width=200)
