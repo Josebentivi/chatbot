8fa1f7e444fe4f2161a41855f6d2eaf3bc3c2f38
João José Bentivi <jjbentivi@hotmail.com>
2025-03-31 03:07:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,122 +57,122 @@ elif st.session_state.openai_api_entered == True:
         if st.button("🗣 Mesa de discussão", use_container_width=True):
             st.session_state.product_page = "mesa_discussao"
     
-if st.session_state.product_page == "chat":
-    openai_api_key = st.session_state.openai_api_key
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    coluna1, coluna2  = st.columns([2, 6], border=True)
-    # Create an OpenAI client.
-    #client = OpenAI(api_key=openai_api_key)
-    with coluna1:
-        st.subheader("Ferramentas")
-        # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
-        col_checkbox, col_info = st.columns([0.6, 0.4])
-        # Cria o checkbox e o ícone de informação na mesma linha
-        with col_checkbox:
-            marcarArtigos = st.checkbox(
-                "Artigos Científicos", 
+    if st.session_state.product_page == "chat":
+        openai_api_key = st.session_state.openai_api_key
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        coluna1, coluna2  = st.columns([2, 6], border=True)
+        # Create an OpenAI client.
+        #client = OpenAI(api_key=openai_api_key)
+        with coluna1:
+            st.subheader("Ferramentas")
+            # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
+            col_checkbox, col_info = st.columns([0.6, 0.4])
+            # Cria o checkbox e o ícone de informação na mesma linha
+            with col_checkbox:
+                marcarArtigos = st.checkbox(
+                    "Artigos Científicos", 
+                    value=False, 
+                    key="marcar_artigos", 
+                    on_change=ativar_artigos
+                )
+            with col_info:
+                # Define o texto que aparecerá ao passar o mouse
+                info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
+                # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
+                st.markdown(
+                    f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
+                    unsafe_allow_html=True
+                )
+
+
+            marcarPensador = st.checkbox(
+                "Pensadores", 
                 value=False, 
-                key="marcar_artigos", 
-                on_change=ativar_artigos
+                key="marcar_pensadores", 
+                on_change=ativar_pensadores
             )
-        with col_info:
-            # Define o texto que aparecerá ao passar o mouse
-            info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
-            # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
-            st.markdown(
-                f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
-                unsafe_allow_html=True
-            )
-
-
-        marcarPensador = st.checkbox(
-            "Pensadores", 
-            value=False, 
-            key="marcar_pensadores", 
-            on_change=ativar_pensadores
-        )
-        
-        if "selected_thinker" not in st.session_state:
-            st.session_state.selected_thinker = None
-
-        if st.session_state.marcar_artigos:
-            st.write("Modo 'Artigos Científicos' ativado.")
-        elif st.session_state.marcar_pensadores:
-            st.session_state.selected_thinker = st.selectbox(
-                "Selecione o pensador:",
-                options=["Sócrates", "Platão", "Aristóteles", "Descartes"],
-                index=["Sócrates", "Platão", "Aristóteles", "Descartes"].index(st.session_state.selected_thinker)
-            )
-            st.write("Modo 'Pensadores' ativado.")
-        else:
-            st.write("Nenhum modo ativo.")            
             
-        if st.session_state.marcar_pensadores:
-            st.info("Artigos desativados", icon="⚠️")
-        if st.session_state.marcar_artigos:
-            st.info("Pensadores desativados", icon="⚠️")
-    with coluna2:
-        # Create a session state variable to store the chat messages. This ensures that the
-        # messages persist across reruns.
+            if "selected_thinker" not in st.session_state:
+                st.session_state.selected_thinker = None
 
-        # Display the existing chat messages via `st.chat_message`.
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-        # Create a chat input field to allow the user to enter a message. This will display
-        # automatically at the bottom of the page.
-    if prompt := st.chat_input("Em que eu posso te ajudar?"):
+            if st.session_state.marcar_artigos:
+                st.write("Modo 'Artigos Científicos' ativado.")
+            elif st.session_state.marcar_pensadores:
+                st.session_state.selected_thinker = st.selectbox(
+                    "Selecione o pensador:",
+                    options=["Sócrates", "Platão", "Aristóteles", "Descartes"],
+                    index=["Sócrates", "Platão", "Aristóteles", "Descartes"].index(st.session_state.selected_thinker)
+                )
+                st.write("Modo 'Pensadores' ativado.")
+            else:
+                st.write("Nenhum modo ativo.")            
+                
+            if st.session_state.marcar_pensadores:
+                st.info("Artigos desativados", icon="⚠️")
+            if st.session_state.marcar_artigos:
+                st.info("Pensadores desativados", icon="⚠️")
+        with coluna2:
+            # Create a session state variable to store the chat messages. This ensures that the
+            # messages persist across reruns.
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+            # Display the existing chat messages via `st.chat_message`.
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+            # Create a chat input field to allow the user to enter a message. This will display
+            # automatically at the bottom of the page.
+        if prompt := st.chat_input("Em que eu posso te ajudar?"):
+
+            # Store and display the current prompt.
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            
+            # Generate a response using the OpenAI API.
+            #stream = client.chat.completions.create(
+            #    model="gpt-3.5-turbo",
+            #    messages=[
+            #        {"role": m["role"], "content": m["content"]}
+            #        for m in st.session_state.messages
+            #    ],
+            #    stream=True,
+            #)
+
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            #with st.chat_message("assistant"):
+            #    response = st.write_stream(stream)
+
+            # Chamada à API (substitua a URL pelo endpoint real)
+            url = "http://52.2.202.37/teste/"
+            data = {"entrada": "string",
+                    "livro": "string",
+                    "historico": "string",
+                    "nivel": "string",
+                    "tema": "string"
+                    }
+            
+            #data = {"cliente": "string",
+            #        "produto": "string",
+            #        }
+            response = requests.post(url, json=data, timeout=5*60)
+            if response.status_code == 200:  
+                saida = response.json()["saida"]
+                #erro = response.json()["erro"]
+            else:  
+                print("Erro na requisição")
+                print(response.status_code)
+                print(response.text) 
+                st.stop()   
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            
+            st.session_state.messages.append({"role": "assistant", "content": saida})
+            with st.chat_message("assistant"):
+                st.markdown(saida)
+                #st.write_stream(saida)
+            
 
         
-        # Generate a response using the OpenAI API.
-        #stream = client.chat.completions.create(
-        #    model="gpt-3.5-turbo",
-        #    messages=[
-        #        {"role": m["role"], "content": m["content"]}
-        #        for m in st.session_state.messages
-        #    ],
-        #    stream=True,
-        #)
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        #with st.chat_message("assistant"):
-        #    response = st.write_stream(stream)
-
-        # Chamada à API (substitua a URL pelo endpoint real)
-        url = "http://52.2.202.37/teste/"
-        data = {"entrada": "string",
-                "livro": "string",
-                "historico": "string",
-                "nivel": "string",
-                "tema": "string"
-                }
-        
-        #data = {"cliente": "string",
-        #        "produto": "string",
-        #        }
-        response = requests.post(url, json=data, timeout=5*60)
-        if response.status_code == 200:  
-            saida = response.json()["saida"]
-            #erro = response.json()["erro"]
-        else:  
-            print("Erro na requisição")
-            print(response.status_code)
-            print(response.text) 
-            st.stop()   
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        
-        st.session_state.messages.append({"role": "assistant", "content": saida})
-        with st.chat_message("assistant"):
-            st.markdown(saida)
-            #st.write_stream(saida)
-        
-
-    
