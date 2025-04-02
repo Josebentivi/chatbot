248063c7f4f566fe010bede697e17c23ebab24ff
João José Bentivi <jjbentivi@hotmail.com>
2025-04-02 17:10:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,26 +196,25 @@ elif st.session_state.usuario:
         # Create an OpenAI client.
         #client = OpenAI(api_key=openai_api_key)
         #    st.subheader("Ferramentas")
-        #    # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
-        #    col_checkbox, col_info = st.columns([0.6, 0.4])
-        #    # Cria o checkbox e o ícone de informação na mesma linha
-        #    with col_checkbox:
-        #        marcarArtigos = st.checkbox(
-        #            "Artigos Científicos", 
-        #            value=False, 
-        #            key="marcar_artigos", 
-        #            on_change=ativar_artigos
-        #        )
-        #    with col_info:
-        #        # Define o texto que aparecerá ao passar o mouse
-        #        info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
-        #        # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
-        #        st.markdown(
-        #            f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
-        #            unsafe_allow_html=True
-        #        )
-        #
-        #
+            # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
+            col_checkbox, col_info = st.columns([0.6, 0.4])
+            # Cria o checkbox e o ícone de informação na mesma linha
+            with col_checkbox:
+                marcarArtigos = st.checkbox(
+                    "Artigos Científicos", 
+                    value=False, 
+                    key="marcar_artigos", 
+                    on_change=ativar_artigos
+                )
+            with col_info:
+                # Define o texto que aparecerá ao passar o mouse
+                info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
+                # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
+                st.markdown(
+                    f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
+                    unsafe_allow_html=True
+                )
+            # Cria o checkbox para o modo "Pensadores" e o ícone de informação na mesma linha
             marcarPensador = st.checkbox(
                 "Pensadores", 
                 value=False, 
