9167f9e0f1942a41b08022b0d162cf8b46f8929c
João José Bentivi <jjbentivi@hotmail.com>
2025-04-11 16:28:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,10 +171,10 @@ if "usuario" not in st.session_state:
     ConfigInicio = st.empty()
     MensagensIniciais = st.empty()
     ChatInterno = st.empty()
-    with ChatInterno.container(height=600,border=False):
+    with ChatInterno.container(height=2500,border=False):
         if prompt := st.chat_input("Em que eu posso te ajudar?",accept_file=False,):
             st.session_state.PrimeitaEntrada = prompt
-            with MensagensIniciais.container(height=200,border=False):   
+            with MensagensIniciais.container(height=600,border=False): 
                 with st.container():
                     st.session_state.usuario = str(uuid.uuid4())
                     st.session_state.selected_model = "gpt-4o-mini"
