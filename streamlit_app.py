2310bcdabf173d8ee355fd8c07669776b54a21eb
João José Bentivi <jjbentivi@hotmail.com>
2025-09-10 20:41:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,6 +206,14 @@ else:
                 key="marcar_artigos", 
                 on_change=ativar_artigos
             )
+        with col_info:
+            # Define o texto que aparecerá ao passar o mouse
+            info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
+            # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
+            st.markdown(
+                f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
+                unsafe_allow_html=True
+            )
         # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
         col_checkbox, col_info = st.columns([0.7, 0.3])
         # Cria o checkbox e o ícone de informação na mesma linha
@@ -220,14 +228,6 @@ else:
         with col_info:
             # Define o texto que aparecerá ao passar o mouse
             info_text = "Tenha uma inteligência artificial treinada nas obras de diversos pensadores. Desbrave o mundo das ideias e encontre respostas para os seus questionamentos mais difíceis."
-            # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
-            st.markdown(
-                f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
-                unsafe_allow_html=True
-            )
-        with col_info:
-            # Define o texto que aparecerá ao passar o mouse
-            info_text = "Com o objetivo de ter um mecanismo de pesquisa imparcial. Desenvolvemos um algoritimo que verifica semânticamente toda nossa base de dados com mais de 220 mil artigos publicados no ano de 2024."
             # O ícone ℹ (código HTML &#9432;) possui o atributo title que exibe o tooltip
             st.markdown(
                 f"<span title='{info_text}' style='cursor: pointer;'>&#9432;</span>",
