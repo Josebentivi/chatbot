be29afa6b56f15a13c6166509aee49af2792b26f
João José Bentivi <jjbentivi@hotmail.com>
2025-09-12 02:20:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,7 +180,7 @@ else:
     if "messages" not in st.session_state:
         #st.text("Carregando mensagens...")
         if "entrouapresentacao" not in st.session_state:
-            textoinicial = """O Pensador é uma ferramenta baseada em inteligência artificial concebida para aprimorar o debate acadêmico e o pensamento crítico. O objetivo central é oferecer um meio pelo qual pesquisadores, estudantes e docentes possam confrontar ideias de forma estruturada, rigorosa e verificável, contrastando argumentos com trechos e referências de obras relevantes e simulando a interação entre diferentes pensadores sobre um mesmo tema."""
+            textoinicial = """O Pensador é uma ferramenta baseada em inteligência artificial concebida para aprimorar o debate acadêmico e o pensamento crítico. O objetivo central é oferecer um meio pelo qual pesquisadores, estudantes e docentes possam confrontar ideias de forma estruturada, rigorosa e verificável, contrastando argumentos com trechos e referências de obras relevantes e simulando a interação entre diferentes pensadores sobre um mesmo tema.\n\n"""
             with apresentacao.container():
                 culunaparesentacao = st.columns([1,2,1],vertical_alignment="center")
                 with culunaparesentacao[1]:
@@ -189,11 +189,11 @@ else:
                     #st.markdown("##### Uma ferramenta para debates acadêmicos")
                     #st.markdown("###### Desenvolvido por João Beneti")
                     st.markdown(f"<div style='text-align: center;'>{textoinicial}</div>", unsafe_allow_html=True)
-            colsapresentacao = st.columns(5)
-            with colsapresentacao[2]:
-                if st.button("Acessar Plataforma", width="stretch", use_container_width=True):
-                    st.session_state.entrouapresentacao = True
-                    st.rerun()
+                colsapresentacao = st.columns(5)
+                with colsapresentacao[2]:
+                    if st.button("Acessar Plataforma", width="stretch", use_container_width=True):
+                        st.session_state.entrouapresentacao = True
+                        st.rerun()
             st.stop()
         st.session_state.messages = Carregando(aceleracao=0.1)
         apresentacao.empty()
