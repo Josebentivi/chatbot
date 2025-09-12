e5a196df985b727af1bb0052705447e6f723bf58
João José Bentivi <jjbentivi@hotmail.com>
2025-09-12 02:08:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,14 +180,15 @@ else:
     if "messages" not in st.session_state:
         #st.text("Carregando mensagens...")
         textoinicial = """O Pensador é uma ferramenta baseada em inteligência artificial concebida para aprimorar o debate acadêmico e o pensamento crítico. O objetivo central do projeto é oferecer um meio pelo qual pesquisadores, estudantes e docentes possam confrontar ideias de forma estruturada, rigorosa e verificável, contrastando argumentos com trechos e referências de obras relevantes e simulando a interação entre diferentes pensadores sobre um mesmo tema."""
-        apresentacao = st.columns([1,2,1],vertical_alignment="center")
-        with apresentacao[1]:
-            #st.markdown("### Bem vindo ao Pensador")
-            #st.markdown("#### A revolução do pensamento crítico")
-            #st.markdown("##### Uma ferramenta para debates acadêmicos")
-            #st.markdown("###### Desenvolvido por João Beneti")
-            st.markdown(f"<div style='text-align: center;'>{textoinicial}</div>", unsafe_allow_html=True)
-        st.session_state.messages = Carregando(aceleracao=0.1)
+        with apresentacao.container():
+            culunaparesentacao = st.columns([1,2,1],vertical_alignment="center")
+            with culunaparesentacao[1]:
+                #st.markdown("### Bem vindo ao Pensador")
+                #st.markdown("#### A revolução do pensamento crítico")
+                #st.markdown("##### Uma ferramenta para debates acadêmicos")
+                #st.markdown("###### Desenvolvido por João Beneti")
+                st.markdown(f"<div style='text-align: center;'>{textoinicial}</div>", unsafe_allow_html=True)
+            st.session_state.messages = Carregando(aceleracao=0.1)
         apresentacao.empty()
     # Sidebar: configurações
     with st.sidebar:
