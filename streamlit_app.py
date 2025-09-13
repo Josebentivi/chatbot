f6b7e590a6918a5c25e46e3a89a6ec0e0fa760a2
João José Bentivi <jjbentivi@hotmail.com>
2025-09-12 23:45:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -253,11 +253,11 @@ else:
         #st.session_state.selected_model = selected
 
         # Cria duas colunas: a primeira para o checkbox e a segunda para o ícone de informação
-        credtstotal,addcredts,vazio = st.columns([0.6,0.2,0.2])
+        credtstotal,addcredts = st.columns([0.6,0.4])
         with credtstotal:
             st.markdown(f"### :green[Creditos: 20]")
         with addcredts:
-            if st.button("", icon=":material/add:", use_container_width=True):
+            if st.button("Loja", icon=":material/add_2:", use_container_width=True):
                 pass
         if st.session_state.selected_model == "gpt-5-nano":
             st.text("Chat Padrão (Gratuito)")
